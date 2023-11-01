d09bb617938e415e5bf0efb100d1d3d1a1186e6a
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-11-01 06:41:48 +0000

    refactor: add, load new media item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
 
 @NgModule({
   imports: [BrowserModule],
   bootstrap: [AppComponent],
-  declarations: []
+  declarations: [
+    AppComponent,
+    MediaItemComponent
+  ]
 })
 export class AppModule { }
